0dd7c7fedc19058d8891d9338a0e5dc07e1afeac
Praise Ebuka <praise.njoga@gmail.com>
2024-01-07 20:32:29 +0100

    time to go :)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in :)!") }}
-
+  
                 </div>
 
                 <div class="p-5 text-gray-800 dark:text-gray-200" style="padding: 30px; font-weight: 700;">
